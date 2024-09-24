9cc8a348b272c75da126342ccf79d8e1e324a455
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-09-24 15:34:55 +0900

    init

---
+++ src/main/java/babycareai/backend/BabyCareAiApplication.java
@@ -0,0 +1,13 @@
+package babycareai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyCareAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyCareAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/babycareai/backend/BabyCareAiApplicationTests.java
@@ -0,0 +1,13 @@
+package babycareai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyCareAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
