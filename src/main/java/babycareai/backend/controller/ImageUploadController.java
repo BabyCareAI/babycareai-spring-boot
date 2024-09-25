ee2b40adaaa3fe90671eed4d5e4a697c84122375
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-09-25 13:42:23 +0900

    refactor: Swagger에서 multipart/form-data 지원을 위한 @PostMapping에 consumes 설정 추가

--- src/main/java/babycareai/backend/controller/ImageUploadController.java
+++ src/main/java/babycareai/backend/controller/ImageUploadController.java
@@ -2,39 +2,47 @@ package babycareai.backend.controller;
 
 import babycareai.backend.service.ImageUploadService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Map;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ImageUploadController {
 
     private final ImageUploadService imageUploadService;
 
-    @Tag(name = "이미지 업로드", description = "이미지를 업로드하는 API")
+    @Tag(name = "이미지 업로드(image)", description = "이미지를 업로드하는 API")
     @Operation(summary = "이미지 업로드", description = "이미지를 업로드합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "이미지 업로드 성공", content = {@Content(mediaType = "application/json")}),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청", content = {@Content(mediaType = "application/json")})
+            @ApiResponse(responseCode = "200", description = "이미지 업로드 성공"),
+            @ApiResponse(responseCode = "500", description = "이미지 업로드 실패")
     })
-    @PostMapping("/upload")
-    public String upload(MultipartFile image, Model model) throws IOException {
-        /* 이미지 업로드 로직 */
-        String imageUrl = imageUploadService.upload(image);
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping(value = "/upload", consumes = {"multipart/form-data"})
+    public ResponseEntity<?> upload(@RequestParam("image") MultipartFile image) { // max-file-size=10MB로 설정시켜놓음.
+        try {
+            // 이미지 업로드 서비스 호출
+            String imageUrl = imageUploadService.upload(image);
 
-        /* View에게 전달할 데이터를 Model에 담음 */
-        model.addAttribute("imageUrl", imageUrl);
+            // 성공적으로 업로드되었을 때의 응답
+            return ResponseEntity.ok().body(Map.of("imageUrl", imageUrl));
 
-        /* View의 이름을 반환하여 View를 렌더링하도록 함 */
-        return "image";
+        } catch (IOException e) {
+            // 오류 응답
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("errorMessage", "이미지 업로드 중 오류가 발생했습니다."));
+        }
     }
 }
