732a8fa4fde6722b23edfa8021588d64ee84cdb5
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-10-06 10:19:04 +0900

    feat: Swagger API 문서의 서버 URL을 동적으로 설정 및 API에 에러 응답 추가

--- src/main/java/babycareai/backend/config/SwaggerConfig.java
+++ src/main/java/babycareai/backend/config/SwaggerConfig.java
@@ -1,12 +1,20 @@
 package babycareai.backend.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${server.url}")
+    private String serverUrl;
+
 
     @Bean
     public OpenAPI openAPI() {
@@ -14,6 +22,7 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("BabyCareAI API")
                         .description("BabyCareAI API를 제공합니다.")
-                        .version("0.0.1"));
+                        .version("0.0.1"))
+                .servers(List.of(new Server().url(serverUrl)));
     }
 }
