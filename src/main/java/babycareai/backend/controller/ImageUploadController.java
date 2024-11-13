df96c0e04fbe985501ab7fe30790ec66611e93ad
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-11-14 02:42:52 +0900

    feat: Redis Stream 방식에서 Redis 저장 방식으로 이미지 예측 결과 처리 수정

--- src/main/java/babycareai/backend/controller/ImageUploadController.java
+++ src/main/java/babycareai/backend/controller/ImageUploadController.java
@@ -1,12 +1,12 @@
 package babycareai.backend.controller;
 
 import babycareai.backend.service.ImageUploadService;
+import babycareai.backend.service.SkinDiseasePredictionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,28 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-public class DiagnosisController {
+public class ImageUploadController {
 
     private final ImageUploadService imageUploadService;
+    private final SkinDiseasePredictionService skinDiseasePredictionService;
 
-    @Tag(name = "진단", description = "이미지를 업로드 -> s3에 저장 -> 예측 모델로 예측 -> 결과를 FastAPI로 전송")
-    @Operation(summary = "진단", description = "이미지를 업로드하면 s3에 저장하고 예측 모델이 이미지를 예측하고 결과를 FastAPI로 전송합니다.")
+    @Tag(name = "이미지 업로드", description = "이미지 업로드 -> 질환 예측 -> 결과 저장")
+    @Operation(summary = "이미지 업로드", description = "증상이 있는 신체 부위를 찍은 이미지를 업로드 하면 이미지 예측 모델이 질환을 예측하고 그 결과를 저장합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "400", description = "잘못된 요청"),
             @ApiResponse(responseCode = "500", description = "서버 에러")
     })
     @CrossOrigin(origins = "${cors.allowedOrigins}")
-    @PostMapping(value = "/api/diagnosis", consumes = {"multipart/form-data"})
-    public ResponseEntity<Void> diagnosis(@RequestParam("image") MultipartFile image) {
-        try {
-            imageUploadService.upload(image);
-            return ResponseEntity.ok().build();
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+    @PostMapping(value = "/api/diagnosis/upload", consumes = {"multipart/form-data"})
+    public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile image) throws IOException {
+        String requestId = UUID.randomUUID().toString();
+        skinDiseasePredictionService.predict(imageUploadService.upload(image), requestId);
+        return ResponseEntity.ok(requestId);
     }
 }
--- src/main/java/babycareai/backend/util/RedisStreamHelper.java
+++ src/main/java/babycareai/backend/util/RedisStreamHelper.java
@@ -1,38 +1,38 @@
-package babycareai.backend.util;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.connection.stream.RecordId;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
-
-import java.util.Map;
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class RedisStreamHelper {
-
-    private final RedisTemplate<String, String> redisTemplate;
-
-    public void trimStream(String streamName, long maxLength) {
-        redisTemplate.opsForStream().trim(streamName, maxLength);
-        }
-
-    public void publishToStream(String streamName, Map<String, String> message) {
-        RecordId recordId = redisTemplate.opsForStream().add(streamName, message);
-        // 스트림 트림
-        trimStream(streamName, 1000);
-
-        if (recordId == null) {
-            log.error("Redis Stream에 메시지를 게시하는 데 실패했습니다.");
-            // 예외 처리 로직 추가
-        } else {
-            log.info("Redis Stream에 메시지를 게시했습니다. recordId: {}", recordId);
-        }
-    }
-
-    // acknowledge 메서드
-    public void acknowledge(String groupName, String consumerName, RecordId recordId) {
-        redisTemplate.opsForStream().acknowledge(groupName, consumerName, recordId);
-    }
-}
+//package babycareai.backend.util;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.data.redis.connection.stream.RecordId;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Map;
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class RedisStreamHelper {
+//
+//    private final RedisTemplate<String, String> redisTemplate;
+//
+//    public void trimStream(String streamName, long maxLength) {
+//        redisTemplate.opsForStream().trim(streamName, maxLength);
+//        }
+//
+//    public void publishToStream(String streamName, Map<String, String> message) {
+//        RecordId recordId = redisTemplate.opsForStream().add(streamName, message);
+//        // 스트림 트림
+//        trimStream(streamName, 1000);
+//
+//        if (recordId == null) {
+//            log.error("Redis Stream에 메시지를 게시하는 데 실패했습니다.");
+//            // 예외 처리 로직 추가
+//        } else {
+//            log.info("Redis Stream에 메시지를 게시했습니다. recordId: {}", recordId);
+//        }
+//    }
+//
+//    // acknowledge 메서드
+//    public void acknowledge(String groupName, String consumerName, RecordId recordId) {
+//        redisTemplate.opsForStream().acknowledge(groupName, consumerName, recordId);
+//    }
+//}
