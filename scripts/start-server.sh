17ccec9facb692f4ad0d82d3202da6a919986468
BaxDailyGit <tmdwls8387@ajou.ac.kr>
2025-05-23 18:04:58 +0900

    chore: 이전 버전의 springboot-server 이미지 삭제 명령어 추가 (release/53-v0.4.0 배포) #53

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -5,4 +5,5 @@ docker stop springboot-server || true
 docker rm springboot-server || true
 docker pull 293337163237.dkr.ecr.ap-northeast-2.amazonaws.com/babycareai/springboot-server:latest
 docker run -d --name springboot-server -p 8080:8080 293337163237.dkr.ecr.ap-northeast-2.amazonaws.com/babycareai/springboot-server:latest
+docker images | grep "springboot-server" | grep -v "latest" | awk '{print $3}' | xargs -r docker rmi -f
 echo "--------------- 서버 배포 완료 -----------------"
