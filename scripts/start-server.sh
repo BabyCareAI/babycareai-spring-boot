d3b69fab06cd663adf15d49dd52a9f09845230a4
BaxDailyGit <tmdwls8387@ajou.ac.kr>
2025-05-27 18:08:00 +0900

    chore: 배포 스크립트에서 배포 시작 메시지 버전 번호 수정 (release/57-v0.4.1 배포) #57

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "--------------- release/57-v0.4.1 배포 시작 (5) -----------------"
+echo "--------------- release/57-v0.4.1 배포 시작 (6) -----------------"
 
 cd /home/ubuntu/babycareai-server || {
   echo "디렉토리 이동 실패: /home/ubuntu/babycareai-server"
