61de3424b311dd81b316ca323ae2a883c9a377e4
BaxDailyGit <tmdwls8387@ajou.ac.kr>
2025-05-27 15:46:07 +0900

    chore: 배포 스크립트 수정 docker compose로 변경 및 디렉토리 이동 추가 (release/57-v0.4.1 배포) #57

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-echo "--------------- release/57-v0.4.1 배포 시작 (2) -----------------"
+echo "--------------- release/57-v0.4.1 배포 시작 (3) -----------------"
 
 # 기존 컨테이너 중지 및 제거
-docker-compose down
+docker compose down
 
 # 최신 이미지 가져오기
 docker pull 293337163237.dkr.ecr.ap-northeast-2.amazonaws.com/babycareai/springboot-server:latest
 
 # 백그라운드로 전체 서비스 시작
-docker-compose up -d
+cd babycareai-server
+docker compose up -d
 
 # 필요 없는 springboot-server 이미지 정리
 docker images | grep "springboot-server" | grep -v "latest" | awk '{print $3}' | xargs -r docker rmi -f
