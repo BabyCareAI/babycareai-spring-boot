7f87dcaf225c3ef1bd78adceaf80338ff6982a9d
BaxDailyGit <tmdwls8387@ajou.ac.kr>
2025-05-30 18:32:19 +0900

    chore: release/53-v0.4.1 배포 #57

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "--------------- release/57-v0.4.1 배포 시작(7) -----------------"
+echo "--------------- release/57-v0.4.1 배포 시작(8) -----------------"
 
 cd /home/ubuntu/babycareai-server || {
   echo "디렉토리 이동 실패: /home/ubuntu/babycareai-server"
